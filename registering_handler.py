175934fbb266ecf1770bf3d898de6f82bbeb59a1
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-04-12 19:33:47 +0200

    Added client side registering_handler that will register the device to the master node

---
+++ registering_handler.py
@@ -0,0 +1,14 @@
+from config_handler import ConfigHandler
+
+
+class RegisteringHandler:
+
+    def __init__(self) -> None:
+
+        self.__config_handler = ConfigHandler()
+
+    def register(self):
+        pass
+
+    def unregister(self):
+        pass
