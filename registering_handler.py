2fdc24b533624b3b27810c15a667c71b0c893282
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-04-19 18:57:59 +0200

    RegisteringHandler is now able to register to the master node

--- registering_handler.py
+++ registering_handler.py
@@ -1,14 +1,30 @@
-from config_handler import ConfigHandler
+from urllib import response
+from network_utils import NetworkHandler
+import requests
 
 
 class RegisteringHandler:
 
     def __init__(self) -> None:
 
-        self.__config_handler = ConfigHandler()
+        self._network_handler = NetworkHandler()
+        self.ip_address: str = self._network_handler.get_ip_address()
+        self.port_number: int = self._network_handler.get_network_port()
+
 
     def register(self):
-        pass
+        master_node_register_url: str = self._network_handler.get_master_node_url('register')
+        payload = self._network_handler.get_registering_payload('aa')
+
+        response = requests.post(url=master_node_register_url, json=payload)
+        
+        
 
     def unregister(self):
         pass
+
+
+if __name__ == '__main__':
+    rh = RegisteringHandler()
+
+    rh.register()
