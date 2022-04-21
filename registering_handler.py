923dde6bd82460f02989f5ab9342ec1e84734397
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-04-21 14:50:32 +0200

    Refactored RegisteringHandler

--- registering_handler.py
+++ registering_handler.py
@@ -9,27 +9,28 @@ class RegisteringHandler:
         self._network_handler = NetworkHandler()
         self.ip_address: str = self._network_handler.get_ip_address()
         self.port_number: int = self._network_handler.get_network_port()
-
+        self.master_node_register_url: str = self._network_handler.get_master_node_url('register')
 
     def register(self):
-        master_node_register_url: str = self._network_handler.get_master_node_url('register')
         payload = self._network_handler.get_registering_payload()
+        response = requests.post(url=self.master_node_register_url, json=payload)
+        print(response.status_code, response.text)
 
-        response = requests.post(url=master_node_register_url, json=payload)
-        print(response.status_code)
+    def update(self):
+        pass
         
         
 
     def unregister(self):
-        master_node_register_url: str = self._network_handler.get_master_node_url('register')
+   
         payload = self._network_handler.get_registering_payload()
 
-        response = requests.delete(url=master_node_register_url, json=payload)
+        response = requests.delete(url=self.master_node_register_url, json=payload)
         print(response.status_code, response.text)
 
 
 if __name__ == '__main__':
     rh = RegisteringHandler()
 
-    rh.register()
-    # rh.unregister()
+    # rh.register()
+    rh.unregister()
