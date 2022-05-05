08ff2306d90047bc87fbfc95803407077e651307
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-05-05 15:04:21 +0200

    Code formatting

--- registering_handler.py
+++ registering_handler.py
@@ -9,28 +9,29 @@ class RegisteringHandler:
         self._network_handler = NetworkHandler()
         self.ip_address: str = self._network_handler.get_ip_address()
         self.port_number: int = self._network_handler.get_network_port()
-        self.master_node_register_url: str = self._network_handler.get_master_node_url('register')
+        self.master_node_register_url: str = self._network_handler.get_master_node_url(
+            'register')
 
     def register(self):
         payload = self._network_handler.get_registering_payload()
-        response = requests.post(url=self.master_node_register_url, json=payload)
+        response = requests.post(
+            url=self.master_node_register_url, json=payload)
         print(response.status_code, response.text)
 
     def update(self):
         pass
-        
-        
 
     def unregister(self):
-   
+
         payload = self._network_handler.get_registering_payload()
 
-        response = requests.delete(url=self.master_node_register_url, json=payload)
+        response = requests.delete(
+            url=self.master_node_register_url, json=payload)
         print(response.status_code, response.text)
 
 
 if __name__ == '__main__':
     rh = RegisteringHandler()
 
-    # rh.register()
-    rh.unregister()
+    rh.register()
+    # rh.unregister()
